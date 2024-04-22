72a1e9f12725f0ab2594946452cb4fcc0d21f307
Bhoomi0207 <bhoomiag0207@gmail.com>
2024-04-23 00:38:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/workliteadmin/addTask2.kt
@@ -0,0 +1,77 @@
+package com.example.workliteadmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.workliteadmin.databinding.ActivityAddTask2Binding
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class addTask2 : AppCompatActivity() {
+    private lateinit var binding: ActivityAddTask2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddTask2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.submitButton.setOnClickListener {
+            validateAndStoreData()
+        }
+    }
+
+    private fun validateAndStoreData() {
+        val taskName = binding.taskame.text.toString()
+        val taskDate = binding.taskdate.text.toString()
+        val checkIn = binding.checkin.text.toString()
+        val checkOut = binding.checkout.text.toString()
+
+        /* if (taskName.isEmpty()) {
+             binding.taskame.requestFocus()
+             binding.taskame.error = "Task Name cannot be empty"
+             return
+         }
+
+         if (taskDate.isEmpty()) {
+             binding.taskdate.requestFocus()
+             binding.taskdate.error = "Task Date cannot be empty"
+             return
+         }*/
+
+
+
+        storeData(taskName, taskDate, checkIn, checkOut)
+    }
+
+    private fun storeData(taskName: String, taskDate: String, checkIn: String, checkOut: String) {
+        val db = Firebase.firestore
+        val productsRef = db.collection("Products")
+
+        val data = addTaskModel(binding.taskame.toString(),
+            binding.taskdate.toString(),
+            binding.checkin.toString(),
+            binding.checkout.toString()
+        )
+
+        productsRef.add(data)
+            .addOnSuccessListener {
+                Toast.makeText(this@addTask2, "Product added", Toast.LENGTH_SHORT).show()
+                clearFields()
+            }
+            .addOnFailureListener { e ->
+                Log.e("FirebaseError", "Error adding product", e)
+                Toast.makeText(
+                    this@addTask2,
+                    "Something went wrong: ${e.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun clearFields() {
+        binding.taskame.text = null
+        binding.taskdate.text = null
+        binding.checkin.text = null
+        binding.checkout.text = null
+    }
+}
---
+++ app/src/main/java/com/example/workliteadmin/addTaskModel.kt
@@ -0,0 +1,11 @@
+package com.example.workliteadmin
+
+data class addTaskModel(
+    val TaskName : String?="",
+    val Taskdate: String?="",
+    val checkin : String?="",
+    val checkout : String?="",
+
+
+
+)
