4186c89c1048930e57858d477dd7109ac61ed291
Bhoomi0207 <bhoomiag0207@gmail.com>
2024-04-25 00:05:25 +0530

    admin

--- app/src/main/java/com/example/workliteadmin/DetailsFragment.kt
+++ app/src/main/java/com/example/workliteadmin/DetailsFragment.kt
@@ -1,80 +1,99 @@
 package com.example.workliteadmin
 
+
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.workliteadmin.databinding.FragmentDetailsBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.util.*
 
 
 class DetailsFragment : Fragment() {
-  private lateinit var binding: FragmentDetailsBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-    }
+
+    private lateinit var binding:FragmentDetailsBinding
+
+
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding=FragmentDetailsBinding.inflate(layoutInflater)
-        binding.submitButton.setOnClickListener {
-            validateAndStoreData()
+        inflater:LayoutInflater, container:ViewGroup?,
+        savedInstanceState:Bundle?,
+    ):View? {
+        binding = FragmentDetailsBinding.inflate(layoutInflater)
+
+
+
+
+
+
+
+        binding.submitButton.setOnClickListener{
+            validateData()
         }
+
 
         return binding.root
     }
-    private fun validateAndStoreData() {
-        val taskName = binding.taskame.text.toString()
-        val taskDate = binding.taskdate.text.toString()
-        val checkIn = binding.taskame.text.toString()
-        val checkOut = binding.taskdate.text.toString()
 
+    private fun validateData() {
+        if (binding.taskame.text.toString().isEmpty()){
+            binding.taskame.requestFocus()
+            binding.taskame.error= "Empty"
+        }else if (binding.taskdate.text.toString().isEmpty()){
+            binding.taskdate.error = "Empty"
 
-
-
-
-
-        storeData(taskName, taskDate, checkIn, checkOut)
+        }else{
+           storeData()
+        }
     }
 
-    private fun storeData(taskName: String, taskDate: String, checkIn: String, checkOut: String) {
-        val db = Firebase.firestore
-        val productsRef = db.collection("Products")
 
-        val data = addTaskModel(binding.taskame.toString(),
-            binding.taskdate.toString(),
-            binding.checkin.toString(),
-            binding.checkout.toString()
+
+    private fun storeData() {
+        val db = Firebase.firestore.collection("Products")
+        val key = db.document().id
+
+        val data = addTaskModel(
+            binding.taskame.text.toString(),
+            binding.taskdate.text.toString(),
+          //  key,
+
         )
+        db.document(key).set(data).addOnSuccessListener {
+           // dialog.dismiss()
 
-        productsRef.add(data)
-            .addOnSuccessListener {
-                Toast.makeText(requireContext(), "Product added", Toast.LENGTH_SHORT).show()
-                clearFields()
-            }
+            Toast.makeText(requireContext(), "sucessfully added", Toast.LENGTH_SHORT).show()
+            binding.taskame.text = null
+        }
             .addOnFailureListener { e ->
-                Log.e("FirebaseError", "Error adding product", e)
+               // dialog.dismiss()
+                Log.e("FirebaseError", "Error adding category", e)
                 Toast.makeText(
                     requireContext(),
                     "Something went wrong: ${e.message}",
                     Toast.LENGTH_SHORT
                 ).show()
+
             }
+
+
     }
 
-    private fun clearFields() {
-        binding.taskame.text = null
-        binding.taskdate.text = null
-        binding.checkin.text = null
-        binding.checkout.text = null
+
+
+
     }
-}
+
+
+
 
 
--- app/src/main/java/com/example/workliteadmin/addTaskModel.kt
+++ app/src/main/java/com/example/workliteadmin/addTaskModel.kt
@@ -3,9 +3,4 @@ package com.example.workliteadmin
 data class addTaskModel(
     val TaskName : String?="",
     val Taskdate: String?="",
-    val checkin : String?="",
-    val checkout : String?="",
-
-
-
     )
